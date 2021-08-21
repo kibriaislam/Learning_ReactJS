de7b6a7320ff25be3f458146b7f740725b33fe26
Kibria Islam <kibriaislam82@gmail.com>
2021-08-22 04:50:48 +0600

    Learn about props

--- src/index.js
+++ src/index.js
@@ -4,27 +4,50 @@ import React from 'react';
 //css
 import  './index.css';
 
+ const books =[
+ {
+
+   title: "Who Will Cry When You Die?",
+   writer: "by Robin Sharma",
+   image : "https://m.media-amazon.com/images/I/81XCK1VLh8L._AC_UY218_.jpg",
+   
+  },
+
+  {
+
+    title : "Malibu Rising: A Novel",
+    writer : "by Taylor Jenkins Reid ",
+    image : "https://m.media-amazon.com/images/I/914BkcGCNgS._AC_UY218_.jpg",
+    
+   },
+
+   {
+      title : "Her Last Holiday",
+      writer : "by C.L. Taylor",
+      image : "https://m.media-amazon.com/images/I/81UHQx0uLuL._AC_UY218_.jpg",
+      
+   },
+
+ ]
+
 
 const BookShop = () =>{
    return(
      <section className='booklist'>
-       <Book/>
-       <Book/>
-       <Book/>
-       <Book/>
-       <Book/>
-       <Book/>
-
+       {books.map((book)=>{
+         
+         return(
+           <Book book={book}></Book>
+         )
+         
+       })}
      </section>
   
-   )
- }
+  )
+}
 
-
-const Book=()=>{
-  let image = "https://m.media-amazon.com/images/I/81XCK1VLh8L._AC_UY218_.jpg";
-  let title = "Who Will Cry When You Die?"
-  let writer = "by Robin Sharma"
+const Book=(props)=>{
+  const {title,writer,image} = props.book;
   return(
     <article className='book'>
       <img src={image} alt="" />
