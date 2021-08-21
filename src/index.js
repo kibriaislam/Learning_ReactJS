a5e52199d9b861f060fb6cbcb39af3f9085865eb
Kibria Islam <kibriaislam82@gmail.com>
2021-08-22 05:18:20 +0600

    Events basics

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import  './index.css';
 
  const books =[
  {
-
+   id:1,
    title: "Who Will Cry When You Die?",
    writer: "by Robin Sharma",
    image : "https://m.media-amazon.com/images/I/81XCK1VLh8L._AC_UY218_.jpg",
@@ -14,7 +14,7 @@ import  './index.css';
   },
 
   {
-
+    id:2,
     title : "Malibu Rising: A Novel",
     writer : "by Taylor Jenkins Reid ",
     image : "https://m.media-amazon.com/images/I/914BkcGCNgS._AC_UY218_.jpg",
@@ -22,6 +22,7 @@ import  './index.css';
    },
 
    {
+      id:3,
       title : "Her Last Holiday",
       writer : "by C.L. Taylor",
       image : "https://m.media-amazon.com/images/I/81UHQx0uLuL._AC_UY218_.jpg",
@@ -37,7 +38,7 @@ const BookShop = () =>{
        {books.map((book)=>{
          
          return(
-           <Book book={book}></Book>
+           <Book key={book.id} book={book}></Book>
          )
          
        })}
@@ -48,11 +49,18 @@ const BookShop = () =>{
 
 const Book=(props)=>{
   const {title,writer,image} = props.book;
+
+  const clickHandler=()=>{
+    alert('clicked')
+  }
+
+
   return(
     <article className='book'>
       <img src={image} alt="" />
       <h1>{title}</h1>
       <p>{writer}</p>
+      <button type="button" onClick={clickHandler}> Click </button>
     </article>
 
   )
